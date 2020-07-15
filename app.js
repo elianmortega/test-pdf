8cb81ab647a8b33d196206a6c60c8de0d5d78e75
elian-ortega <elian.ortega.ucr@gmail.com>
2020-07-15 11:42:57 -0600

    Add products

--- app.js
+++ app.js
@@ -12,12 +12,27 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 const listaProductos = [
-  {nombre : "Alchol 30ml", id:"al30"},
-  { nombre : "Alchol 60ml", id:"al60"},
-  {nombre : "Alchol 90ml", id:"al90"},
-  {nombre : "Alchol 100ml", id:"al100"},
+  {nombre : "Alcohol Gel al 70%", id:"alGel"},
+  {nombre : "Amonio Cuaternario \n4ta Generacion\n", id:"amonio"},
+  {nombre : "Jabon Liquido", id:"jabon"},
+  {nombre : "Alcohol Antiseptico al \n70% con Atomizador\n", id:"alAtom"},
+  {nombre : "Alcohol Antiseptico al 70%", id:"al"},
+  {nombre : "Atomizador con envase", id:"atomEnvase"},
 ]
 
+const listaCapacidades = [
+  {nombre: "30ml", valor: 30},
+  {nombre: "100ml", valor: 100},
+  {nombre: "150ml", valor: 150},
+  {nombre: "200ml", valor: 200},
+  {nombre: "300ml", valor: 300},
+  {nombre: "500ml", valor: 500},
+  {nombre: "600ml", valor: 600},
+  {nombre: "1000ml", valor: 1000},
+  {nombre: "Medio Galon", valor: 1500},
+  {nombre: "Galon", valor: 300},
+  {nombre: "Caja de 50 Unidades", valor: 300},
+];
 
 const { createInvoice } = require("./createInvoice.js");
 
@@ -27,14 +42,16 @@ app.get("/", (req,res)=>{
     // createInvoice(invoice,"invoice.pdf");
     res.render("home", {
       listaProductos: listaProductos,
+      listaCapacidades: listaCapacidades,
     });
     // res.sendFile(__dirname + "/index.html");
 });
 
 
 app.post("/invoice", async (req,res)=>{
+  console.log(req.body);
   var map = req.body;
-    console.log(map);
+    // console.log(map);
     var invoice = await crearProforma(req.body);
     await createInvoice(invoice,"invoice.pdf");
     setTimeout(function () {
@@ -53,16 +70,21 @@ app.listen(PORT, ()=>{
 function crearProforma(userForm){
   // console.log(isNaN(parseInt(userForm["al30"])));
   let itemList = Array();
+  var total = 0;
   //Agregar a itemList
   listaProductos.forEach((producto)=>{
-    let cantidad = parseInt(userForm[producto.id]);
+    let cantidad = parseInt(userForm["cant"+producto.id]);
+    let precio = parseInt(userForm["precio"+producto.id]);
+    let descripcion = userForm["select"+producto.id]
     if(!isNaN(cantidad)){
       let newItem = {
         item: producto.nombre,
-        description: "Descripcion",
+        description: descripcion,
         quantity: cantidad,
-        amount: 0
+        price: precio,
+        total: precio*cantidad
       };
+      total += precio*cantidad;
       itemList.push(newItem);
     }
   });
@@ -77,9 +99,7 @@ function crearProforma(userForm){
       postal_code: 94111
     },
     items: itemList,
-    subtotal: 8000,
-    paid: 0,
-    invoice_nr: 1234
+    total: total,
   };
   return invoice;
 }
--- createInvoice.js
+++ createInvoice.js
@@ -1,12 +1,15 @@
 const fs = require("fs");
 const PDFDocument = require("pdfkit");
 
+var globalPosition = 0;
+
 function createInvoice(invoice, path) {
   let doc = new PDFDocument({ size: "A4", margin: 50 });
 
   generateHeader(doc);
   generateCustomerInformation(doc, invoice);
   generateInvoiceTable(doc, invoice);
+  generateExtraInformation(doc);
   generateFooter(doc);
 
   doc.end();
@@ -20,132 +23,161 @@ function generateHeader(doc) {
     .fontSize(20)
     .text("Cosmeticos Orvel", 110, 57)
     .fontSize(10)
-    .text("Cosmeticos Orvel", 200, 50, { align: "right" })
-    .text("Managua", 200, 65, { align: "right" })
-    .text("Managua, Nicaragua", 200, 80, { align: "right" })
+    .text("ANTIGUA CINERUIZ 1 1/2 C. OESTE", 200, 50, { align: "right" })
+    .text(formatDate(new Date()), 200, 65, { align: "right" })
+    .text("MANAGUA, NICARAGUA", 200, 80, { align: "right" })
     .moveDown();
 }
 
 function generateCustomerInformation(doc, invoice) {
+
+  // const customerInformationTop = 200;
+
+  // doc
+  //   .fontSize(10)
+  //   .text("Invoice Number:", 50, customerInformationTop)
+  //   .font("Helvetica-Bold")
+  //   .text(invoice.invoice_nr, 150, customerInformationTop)
+  //   .font("Helvetica")
+  //   .text("Invoice Date:", 50, customerInformationTop + 15)
+  //   .text(formatDate(new Date()), 150, customerInformationTop + 15)
+  //   .text("Balance Due:", 50, customerInformationTop + 30)
+  //   .text(
+  //     formatCurrency(invoice.subtotal - invoice.paid),
+  //     150,
+  //     customerInformationTop + 30
+  //   )
+
+  //   .font("Helvetica-Bold")
+  //   .text(invoice.shipping.name, 300, customerInformationTop)
+  //   .font("Helvetica")
+  //   .text(invoice.shipping.address, 300, customerInformationTop + 15)
+  //   .text(
+  //     invoice.shipping.city +
+  //       ", " +
+  //       invoice.shipping.state +
+  //       ", " +
+  //       invoice.shipping.country,
+  //     300,
+  //     customerInformationTop + 30
+  //   )
+  //   .moveDown();
+
+  // generateHr(doc, 252);
+
   doc
     .fillColor("#444444")
-    .fontSize(20)
-    .text("Proforma", 50, 160);
+    .fontSize(12)
+    .text(invoice.shipping.name.toUpperCase(), 50, 160);
 
   generateHr(doc, 185);
 
-  const customerInformationTop = 200;
-
-  doc
-    .fontSize(10)
-    .text("Invoice Number:", 50, customerInformationTop)
-    .font("Helvetica-Bold")
-    .text(invoice.invoice_nr, 150, customerInformationTop)
-    .font("Helvetica")
-    .text("Invoice Date:", 50, customerInformationTop + 15)
-    .text(formatDate(new Date()), 150, customerInformationTop + 15)
-    .text("Balance Due:", 50, customerInformationTop + 30)
-    .text(
-      formatCurrency(invoice.subtotal - invoice.paid),
-      150,
-      customerInformationTop + 30
-    )
-
-    .font("Helvetica-Bold")
-    .text(invoice.shipping.name, 300, customerInformationTop)
-    .font("Helvetica")
-    .text(invoice.shipping.address, 300, customerInformationTop + 15)
-    .text(
-      invoice.shipping.city +
-        ", " +
-        invoice.shipping.state +
-        ", " +
-        invoice.shipping.country,
-      300,
-      customerInformationTop + 30
-    )
-    .moveDown();
-
-  generateHr(doc, 252);
+  
 }
 
 function generateInvoiceTable(doc, invoice) {
   let i;
-  const invoiceTableTop = 330;
-
+  const invoiceTableTop = 200;
   doc.font("Helvetica-Bold");
+  doc.fontSize(10);
   generateTableRow(
     doc,
     invoiceTableTop,
-    "Item",
-    "Description",
-    "Unit Cost",
-    "Quantity",
-    "Line Total"
+    "PRODUCTO",
+    "CAPACIDAD",
+    "PRECIO",
+    "CANTIDAD",
+    "TOTAL"
   );
   generateHr(doc, invoiceTableTop + 20);
   doc.font("Helvetica");
 
   for (i = 0; i < invoice.items.length; i++) {
     const item = invoice.items[i];
-    const position = invoiceTableTop + (i + 1) * 30;
+    const position = invoiceTableTop + (i + 1) * 35;
     generateTableRow(
       doc,
       position,
       item.item,
       item.description,
-      formatCurrency(item.amount / item.quantity),
+      formatCurrency(item.price),
       item.quantity,
-      formatCurrency(item.amount)
+      formatCurrency(item.total)
     );
 
-    generateHr(doc, position + 20);
+    generateHr(doc, position + 25);
   }
 
-  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
-  generateTableRow(
-    doc,
-    subtotalPosition,
-    "",
-    "",
-    "Subtotal",
-    "",
-    formatCurrency(invoice.subtotal)
-  );
-
-  const paidToDatePosition = subtotalPosition + 20;
-  generateTableRow(
-    doc,
-    paidToDatePosition,
-    "",
-    "",
-    "Paid To Date",
-    "",
-    formatCurrency(invoice.paid)
-  );
-
-  const duePosition = paidToDatePosition + 25;
+  const totalPosition = invoiceTableTop + (i + 1) * 35;
+  globalPosition = totalPosition + 50;
   doc.font("Helvetica-Bold");
   generateTableRow(
     doc,
-    duePosition,
+    totalPosition,
     "",
     "",
-    "Balance Due",
+    "TOTAL",
     "",
-    formatCurrency(invoice.subtotal - invoice.paid)
+    formatCurrency(invoice.total)
   );
+
+  // const paidToDatePosition = subtotalPosition + 20;
+  // generateTableRow(
+  //   doc,
+  //   paidToDatePosition,
+  //   "",
+  //   "",
+  //   "Paid To Date",
+  //   "",
+  //   formatCurrency(invoice.paid)
+  // );
+
+  // const duePosition = paidToDatePosition + 25;
+  // doc.font("Helvetica-Bold");
+  // generateTableRow(
+  //   doc,
+  //   duePosition,
+  //   "",
+  //   "",
+  //   "Balance Due",
+  //   "",
+  //   formatCurrency(invoice.subtotal - invoice.paid)
+  // );
   doc.font("Helvetica");
+}
+
+function generateExtraInformation(doc){
+  doc.font("Helvetica");
+  doc
+  .fontSize(11)
+  .text("FORMA DE PAGO: CHEQUE, CONTADO O TRANSFERENCIA",50, globalPosition)
+  .text("ENTREGA 5 DIA DESPUÉS DE RECIBIDO EL 50% DE ANTICIPO, 50% CONTRA")
+  .text("ENTREGA PROFORMA VALIDA POR 15 DÍAS");
+
+  doc.font("Helvetica-Bold");
+  doc
+  .fontSize(11)
+  .text("")
+  .text("HACEMOS ENVÍO POR TRANSCARGO", 50)
+  .text("ENVÍO BUS INTER LOCAL")
+  .text("COSTO ADICIONAL")
+  .text("NOTA: SOMOS CUTA FIJA",{underline:true});
+
+  globalPosition+=100;
 }
 
 function generateFooter(doc) {
   doc
-    .fontSize(10)
+    .fontSize(8)
+    .fillColor("red")
     .text(
-      "Payment is due within 15 days. Thank you for your business.",
+      "Telefono: 8264 - 4263",
       50,
-      780,
-      { align: "center", width: 500 }
+      globalPosition,
+      { align: "center", width: 500}
+    )
+    .text(
+      "email: laboratorioorvel@gmail.com",{align:"center"}
     );
 }
 
@@ -161,7 +193,7 @@ function generateTableRow(
   doc
     .fontSize(10)
     .text(item, 50, y)
-    .text(description, 150, y)
+    .text(description, 170, y)
     .text(unitCost, 280, y, { width: 90, align: "right" })
     .text(quantity, 370, y, { width: 90, align: "right" })
     .text(lineTotal, 0, y, { align: "right" });
@@ -176,8 +208,8 @@ function generateHr(doc, y) {
     .stroke();
 }
 
-function formatCurrency(cents) {
-  return "$" + (cents / 100).toFixed(2);
+function formatCurrency(number) {
+  return "C$" + (number).toFixed(2);
 }
 
 function formatDate(date) {
