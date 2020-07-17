f0da8bb07683e0f4f7bcebac6600c976f1ed2c89
elian-ortega <elian.ortega.ucr@gmail.com>
2020-07-17 12:28:54 -0600

    Change select product, add comma decimals

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var PORT = process.env.PORT || 3000;
 const express =  require("express");
 const fs = require("fs");
 const bodyParser = require("body-parser");
-const eja = require("ejs");
+const ejs = require("ejs");
 
 const app = express();
 
@@ -11,28 +11,32 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const listaProductos = [
-  {nombre : "Alcohol Gel al 70%", id:"alGel"},
-  {nombre : "Amonio Cuaternario \n4ta Generacion\n", id:"amonio"},
-  {nombre : "Jabon Liquido", id:"jabon"},
-  {nombre : "Alcohol Antiseptico al \n70% con Atomizador\n", id:"alAtom"},
-  {nombre : "Alcohol Antiseptico al 70%", id:"al"},
-  {nombre : "Atomizador con envase", id:"atomEnvase"},
-]
+var cantidadProductos = 10;
 
-const listaCapacidades = [
-  {nombre: "30ml", valor: 30},
-  {nombre: "100ml", valor: 100},
-  {nombre: "150ml", valor: 150},
-  {nombre: "200ml", valor: 200},
-  {nombre: "300ml", valor: 300},
-  {nombre: "500ml", valor: 500},
-  {nombre: "600ml", valor: 600},
-  {nombre: "1000ml", valor: 1000},
-  {nombre: "Medio Galon", valor: 1500},
-  {nombre: "Galon", valor: 300},
-  {nombre: "Caja de 50 Unidades", valor: 300},
-];
+const listaProductos = {
+  "alcoholGel" : "Alcohol Gel al 70%",
+  "amoniocuaternario": "Amonio Cuaternario \n4ta Generacion\n",
+  "jabonLiquido": "Jabon Liquido",
+  "alcoholAtomizador": "Alcohol Antiseptico al \n70% con Atomizador\n",
+  "alcohol": "Alcohol Antiseptico al 70%",
+  "envaseAtomizador":  "Envase con Atomizador",
+};
+
+
+
+const listaCapacidades = {
+   "30ml":"30ml",
+   "100ml":"100ml",
+   "150ml":"150ml",
+   "200ml":"200ml",
+   "300ml":"300ml",
+   "500ml":"500ml",
+   "600ml":"600ml",
+   "1000ml": "1000ml",
+   "medioGalon": "Medio Galon",
+   "galon":"Galon",
+   "cajaGuantes":"Caja de Guantes 50 Unidades",
+};
 
 const { createInvoice } = require("./createInvoice.js");
 
@@ -43,6 +47,7 @@ app.get("/", (req,res)=>{
     res.render("home", {
       listaProductos: listaProductos,
       listaCapacidades: listaCapacidades,
+      cantidadProductos: cantidadProductos
     });
     // res.sendFile(__dirname + "/index.html");
 });
@@ -51,7 +56,7 @@ app.get("/", (req,res)=>{
 app.post("/invoice", async (req,res)=>{
   console.log(req.body);
   var map = req.body;
-    // console.log(map);
+    console.log(map);
     var invoice = await crearProforma(req.body);
     await createInvoice(invoice,"invoice.pdf");
     setTimeout(function () {
@@ -72,14 +77,16 @@ function crearProforma(userForm){
   let itemList = Array();
   var total = 0;
   //Agregar a itemList
-  listaProductos.forEach((producto)=>{
-    let cantidad = parseInt(userForm["cant"+producto.id]);
-    let precio = parseInt(userForm["precio"+producto.id]);
-    let descripcion = userForm["select"+producto.id]
-    if(!isNaN(cantidad)){
+
+  for(var i=0; i<cantidadProductos;i++){
+    let productoKey = userForm["nombreProducto"+i]
+    let capacidadKey = userForm["capacidadProducto"+i];
+    let cantidad = parseInt(userForm["cantidadProducto"+i]);
+    let precio = parseInt(userForm["precioProducto"+i]);
+    if(!isNaN(cantidad) && !isNaN(precio)){
       let newItem = {
-        item: producto.nombre,
-        description: descripcion,
+        item: listaProductos[productoKey],
+        description: listaCapacidades[capacidadKey],
         quantity: cantidad,
         price: precio,
         total: precio*cantidad
@@ -87,8 +94,9 @@ function crearProforma(userForm){
       total += precio*cantidad;
       itemList.push(newItem);
     }
-  });
+  }
 
+  console.log(itemList);
   var invoice = {
     shipping: {
       name: userForm.nombreCliente,
--- createInvoice.js
+++ createInvoice.js
@@ -217,7 +217,20 @@ function generateHr(doc, y) {
 }
 
 function formatCurrency(number) {
-  return "C$" + (number).toFixed(2);
+  var numberResult = addCommas(number);
+  return "C$ " + numberResult + ".00";
+}
+
+function addCommas(nStr) {
+  nStr += '';
+  var x = nStr.split('.');
+  var x1 = x[0];
+  var x2 = x.length > 1 ? '.' + x[1] : '';
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+          x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+  return x1 + x2;
 }
 
 function formatDate(date) {
